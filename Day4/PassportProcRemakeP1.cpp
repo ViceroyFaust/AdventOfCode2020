85faf256d792d66b9fae492ba39244e768c46247
Danylo Owl <d.ribchinski@gmail.com>
2021-11-13 18:12:05 -0500

    Remade Day 4 Part 1

--- Day4/PassportProcRemakeP1.cpp
+++ Day4/PassportProcRemakeP1.cpp
@@ -1,7 +1,7 @@
-#include <string>
-#include <iostream>
-#include <vector>
 #include <algorithm>
+#include <iostream>
+#include <string>
+#include <vector>
 
 std::vector<std::string> splitString(const std::string& str, std::string delim = " ") {
     typedef std::string::size_type index;
@@ -19,11 +19,11 @@ std::vector<std::string> splitString(const std::string& str, std::string delim =
 
 bool checkPassport(const std::string& passport) {
     std::vector<std::string> fields = splitString(passport);
-    if (fields.size() != 7)
+    if (fields.size() < 7)
         return false;
 
-    auto i = std::find_if(fields.begin(), fields.end(), [](const std::string& str)->bool {
-        if(str.find("cid") == std::string::npos) return false;
+    auto i = std::find_if(fields.begin(), fields.end(), [](const std::string& str) -> bool {
+        if (str.find("cid") == std::string::npos) return false;
         return true;
     });
     if (i != fields.end())
@@ -32,6 +32,21 @@ bool checkPassport(const std::string& passport) {
 }
 
 int main() {
-    
+    std::string input;
+    std::string passport;
+    int validCount = 0;
+    while (std::getline(std::cin, input)) {
+        if (input == "") {
+            // do substr to remove hanging space
+            if (checkPassport(passport.substr(0, passport.size() - 1)))
+                ++validCount;
+            passport = "";
+        } else {
+            passport += input + ' ';
+        }
+    }
+    if (checkPassport(passport.substr(0, passport.size() - 1)))
+        ++validCount;
+    std::cout << validCount << std::endl;
     return 0;
 }
