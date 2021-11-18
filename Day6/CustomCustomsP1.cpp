beb0ed685cb8cdddb44017b3a8972c6f1a6ca969
Danylo Owl <d.ribchinski@gmail.com>
2021-11-17 22:27:33 -0500

    D6 P1 Finished

--- Day6/CustomCustomsP1.cpp
+++ Day6/CustomCustomsP1.cpp
@@ -1,5 +1,26 @@
 #include <iostream>
+#include <set>
+
+int countChoices(const std::string& group) {
+    std::set<char> choices;
+    for (char c : group) {
+        choices.insert(c);
+    }
+    return choices.size();
+}
 
 int main() {
+    int sum = 0;
+    std::string input;
+    std::string group;
+    while (std::getline(std::cin, input)) {
+        if (input == "") {
+            sum += countChoices(group);
+            group = "";
+        } else 
+            group += input;
+    }
+    sum += countChoices(group);
+    std::cout << sum << std::endl;
     return 0;
 }
