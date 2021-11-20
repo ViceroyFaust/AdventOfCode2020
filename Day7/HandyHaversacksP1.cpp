d923ed1ab4a9b4cd7a4a9862892d88500ef7da28
Danylo Owl <d.ribchinski@gmail.com>
2021-11-19 19:38:02 -0500

    D7 made something that works with pointers

--- Day7/HandyHaversacksP1.cpp
+++ Day7/HandyHaversacksP1.cpp
@@ -1,7 +1,5 @@
 #include <iostream>
-#include <set>
 #include <string>
-#include <unordered_map>
 #include <vector>
 
 struct Bag {
@@ -23,18 +21,34 @@ std::vector<std::string> splitString(const std::string& str, std::string delim =
     return output;
 }
 
-std::pair<std::string, int> extractPair(const std::string& word) {
+std::pair<bool, Bag*> bagExists(const std::vector<Bag*>& list, const std::string& key) {
+    for (Bag* b : list) {
+        if (b->name == key)
+            return std::pair<bool, Bag*>(true, b);
+    }
+    return std::pair<bool, Bag*>(false, NULL);
+}
+
+std::pair<int, Bag*> extractPair(const std::string& word, const std::vector<Bag*>& bagList) {
     std::vector<std::string> words = splitString(word);
     int num = std::stoi(words[0]);
     std::string bagType = words[1] + " " + words[2];
-    return std::pair<std::string, int>(bagType, num);
+    Bag* bag;
+    auto check = bagExists(bagList, bagType);
+    if (check.first) {
+        bag = check.second;
+    } else {
+        bag = new Bag;
+        bag->name = bagType;
+    }
+    return std::pair<int, Bag*>(num, bag);
 }
 
-std::set<std::pair<std::string, int>> extractContainedBags(const std::string& word) {
-    std::set<std::pair<std::string, int>> output;
+std::vector<std::pair<int, Bag*>> extractContainedBags(const std::string& word, const std::vector<Bag*>& bagList) {
+    std::vector<std::pair<int, Bag*>> output;
     std::vector<std::string> bags = splitString(word, ", ");
     for (std::string str : bags) {
-        output.insert(extractPair(str));
+        output.push_back(extractPair(str, bagList));
     }
     return output;
 }
@@ -44,23 +58,23 @@ std::string extractKey(const std::string& word) {
     return keyWordSplit[0] + " " + keyWordSplit[1];  // Key is the first two words before "contain"
 }
 
-bool hasBag(const std::unordered_map<std::string, std::set<std::pair<std::string, int>>>& containedBags) {
-    
-    return false;
-}
-
 int main() {
-    // Map containing sets of string and int
-    std::unordered_map<std::string, std::set<std::pair<std::string, int>>> rules;
+    std::vector<Bag*> bagsList;
     std::string input;
-    // Collect all bag rules and store them in a map
     while (std::getline(std::cin, input)) {
-        std::vector<std::string> containSplit = splitString(input, " contain ");
-        std::string key = extractKey(containSplit[0]);
-        std::set<std::pair<std::string, int>> bagSet = extractContainedBags(containSplit[1]);
+        auto splitContain = splitString(input, " contain ");
+        std::string key;
+        if (bagExists) {
+        } else {
+            Bag* bag = new Bag;
+            bagsList.push_back(bag);
+            bag->name = extractKey(splitContain[0]);
+            auto insideBags = extractContainedBags(splitContain[1], bagsList);
+            bag->bags = insideBags;
+            for (std::pair<int, Bag*> p : bag->bags) {
+                bagsList.push_back(p.second);
+            }
+        }
     }
-    int counter = 0;
-    if (hasBag(rules))
-        counter++;
     return 0;
 }
