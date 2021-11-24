e578d71afaac013b299edcb7b59f150d2a8e30d6
Danylo Owl <d.ribchinski@gmail.com>
2021-11-24 13:20:34 -0500

    D9 Finished isError and FindError
    The first draft of checking for the error number is now complete

--- Day9/EncodingErrorP1.cpp
+++ Day9/EncodingErrorP1.cpp
@@ -3,12 +3,14 @@
 #include <string>
 #include <vector>
 
+// Populates a vector with the contents of the input
 void getInput(std::vector<int>& inputs) {
     std::string inputStr;
     while (std::getline(std::cin, inputStr))
         inputs.push_back(std::stoi(inputStr));
 }
 
+// Generates a variable length preamble queue from the input
 std::deque<int> genPreamble(const std::vector<int>& inputs, int len) {
     std::deque<int> preamble;
     for (int i = 0; i < len; ++i) {
@@ -17,9 +19,33 @@ std::deque<int> genPreamble(const std::vector<int>& inputs, int len) {
     return preamble;
 }
 
+bool isError(const std::deque<int>& preamble, int num) {
+    for (size_t i = 0; i < preamble.size(); ++i) {
+        for (size_t j = i + 1; j < preamble.size(); ++j) {
+            if (preamble[i] + preamble[j] == num) {
+                return false;
+            }
+        }
+    }
+    return true;
+}
+
+int findError(const std::vector<int>& inputs, int preambleLen) {
+    std::deque<int> preamble = genPreamble(inputs, preambleLen);
+    for (size_t i = preambleLen; i < inputs.size(); ++i) {
+        int toCheck = inputs[i];
+        if(isError(preamble, toCheck))
+            return toCheck;
+        preamble.pop_front();
+        preamble.push_back(toCheck);
+    }
+    return 0;
+}
+
 int main() {
     std::vector<int> inputList;
     getInput(inputList);
-    std::deque<int> preamble = genPreamble(inputList, 5);
+    
+    std::cout << findError(inputList, 5) << std::endl;
     return 0;
 }
