679520d841504b572fea19c133e149632272f09b
Danylo Owl <d.ribchinski@gmail.com>
2021-11-24 12:54:30 -0500

    D9 Created input methods

--- Day9/EncodingErrorP1.cpp
+++ Day9/EncodingErrorP1.cpp
@@ -1,6 +1,25 @@
+#include <deque>
 #include <iostream>
+#include <string>
+#include <vector>
+
+void getInput(std::vector<int>& inputs) {
+    std::string inputStr;
+    while (std::getline(std::cin, inputStr))
+        inputs.push_back(std::stoi(inputStr));
+}
+
+std::deque<int> genPreamble(const std::vector<int>& inputs, int len) {
+    std::deque<int> preamble;
+    for (int i = 0; i < len; ++i) {
+        preamble.push_back(inputs[i]);
+    }
+    return preamble;
+}
 
 int main() {
-    std::cout << "Hello World!" << std::endl;
-    return 0
+    std::vector<int> inputList;
+    getInput(inputList);
+    std::deque<int> preamble = genPreamble(inputList, 5);
+    return 0;
 }
